5f687c384c6a186d82e92687ee6c9b6ae08ec1cf
1sfandyor <isfandiyor06112000@gmail.com>
2024-11-08 15:53:39 +0500

    dsadasd

--- config/fonts.ts
+++ config/fonts.ts
@@ -64,7 +64,7 @@ export const sourceSansPro = localFont({
     { path: '../public/fonts/SourceSansPro-Bold.woff2', weight: '700' },
     { path: '../public/fonts/SourceSansPro-Regular.woff2', weight: '400' },
     { path: '../public/fonts/SourceSansPro-Light.woff2', weight: '300' },
-    { path: '../public/fonts/SourceSansPro-SemiBold.woff2', weight: '600' },
+    // { path: '../public/fonts/SourceSansPro-SemiBold.woff2', weight: '600' },
   ],
   style: 'normal',
   variable: '--font-source-sans-pro',
