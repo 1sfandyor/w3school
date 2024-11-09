b2ee77a27779fcd071066bfcca3377223719d317
1sfandyor <isfandiyor06112000@gmail.com>
2024-11-09 13:29:49 +0500

    DASDSAD

--- components/HeaderStacks.tsx
+++ components/HeaderStacks.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
 import { stacks } from '@/data/nav'
+import clsx from 'clsx'
+import { sourceSansPro } from '@/config/fonts'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faBars } from '@fortawesome/free-solid-svg-icons'
 
@@ -12,7 +14,7 @@ const HeaderStacks = () => {
         {
           stacks.map((stack) => (
             <li key={stack.id} className=''>
-              <Link className='px-[15px] py-[5px] text-[15px] flex items-center justify-center hover:bg-black-1 hover:text-white-2' href={stack.path} >{stack.name}</Link>
+              <Link className={clsx("px-[15px] py-[5px] text-[15px] flex items-center justify-center hover:bg-black-1 hover:text-white-2", sourceSansPro.className)} href={stack.path} >{stack.name}</Link>
             </li>
           ))
         }
